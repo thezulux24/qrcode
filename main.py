ba627dda9ff80acebd782174a12452c867c0ffd9
thezulux24 <thezulux24gmail.com>
2024-09-30 14:07:53 -0500

    app

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ import streamlit as st
 import qrcode
 from io import BytesIO
 import base64
+from PIL import Image
 
-def generate_qr(data):
+def generate_qr(data, logo=None):
     qr = qrcode.QRCode(
         version=1,
         error_correction=qrcode.constants.ERROR_CORRECT_H,  # Highest error correction level
@@ -12,17 +13,25 @@ def generate_qr(data):
     )
     qr.add_data(data)
     qr.make(fit=True)
-    image = qr.make_image(fill='black', back_color='white')
+    image = qr.make_image(fill='black', back_color='white').convert('RGB')
+
+    if logo:
+        logo = Image.open(logo)
+        logo = logo.resize((image.size[0] // 3, image.size[1] // 3))
+        pos = ((image.size[0] - logo.size[0]) // 2, (image.size[1] - logo.size[1]) // 2)
+        image.paste(logo, pos, mask=logo)
+
     return image
 
 st.title("QR Generator")
 st.write("Simple and Secure")
 
 data = st.text_input("Enter the link for the QR code:")
+logo = st.file_uploader("Upload a logo (optional)", type=["png", "jpg", "jpeg"])
 
 if st.button("Generate QR Code"):
     if data:
-        img = generate_qr(data)
+        img = generate_qr(data, logo)
         buffer = BytesIO()
         img.save(buffer, format="PNG")
         img_base64 = base64.b64encode(buffer.getvalue()).decode()
